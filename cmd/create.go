ab9354254220425ca1f0f21eaddccde8bf69d588
Siddharth Kannan <mail@siddharthkannan.in>
2022-08-15 18:15:26 +0900

    Write a working create command
    Error case: (Not providing the authorization token to the command will cause an error)
    
    	$ go run main.go create \
    		--from "2022-08-22T10:00:00+09:00" \
    		--to "2022-08-25T00:00:00+09:00" \
    		--shift-length "12h" \
    		--shift-days 2 \
    		--gap-days 2 \
    		--user-id "UID" \
    		--schedule-id "SID"
    	2022-08-22T10:00:00+09:00        ->     2022-08-22T22:00:00+09:00       ERROR   HTTP response with status code 401 does not contain Content-Type: application/json
    	2022-08-23T10:00:00+09:00        ->     2022-08-23T22:00:00+09:00       ERROR   HTTP response with status code 401 does not contain Content-Type: application/json
    
    Dry-run:
    
    	$ AUTHORIZATION_TOKEN=$(pass show work/pagerduty/user-api-token) go run main.go create \
    									--from "2022-08-22T10:00:00+09:00" \
    									--to "2022-08-25T00:00:00+09:00" \
    									--shift-length "12h" \
    									--shift-days 2 \
    									--gap-days 2 \
    									--user-id "PP9LMAG" \
    									--schedule-id "PU3N3E4" \
    									--dry-run
    	2022-08-22T10:00:00+09:00        ->     2022-08-22T22:00:00+09:00       DRY-RUN
    	2022-08-23T10:00:00+09:00        ->     2022-08-23T22:00:00+09:00       DRY-RUN
    
    Non-dry-run case:
    
    	$ AUTHORIZATION_TOKEN=$(pass show work/pagerduty/user-api-token) go run main.go create \
    									--from "2022-08-22T10:00:00+09:00" \
    									--to "2022-08-25T00:00:00+09:00" \
    									--shift-length "12h" \
    									--shift-days 2 \
    									--gap-days 2 \
    									--user-id "PP9LMAG" \
    									--schedule-id "PU3N3E4" \
    	2022-08-22T10:00:00+09:00        ->     2022-08-22T22:00:00+09:00       OVERRIDE-ID-1
    	2022-08-23T10:00:00+09:00        ->     2022-08-23T22:00:00+09:00       OVERRIDE-ID-2
    
    After running this, I confirmed from the PagerDuty GUI that the override was actually created.

---
+++ cmd/create.go
@@ -0,0 +1,99 @@
+/*
+Copyright © 2022 Siddharth Kannan <mail@siddharthkannan.in>
+
+*/
+package cmd
+
+import (
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/icyflame/pagerduty-override-management/internal/pagerduty"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	var from, to string
+	var userID, scheduleID string
+	var shiftLength time.Duration
+	var shiftDays, gapDays int
+	var dryRun bool
+
+	// createCmd represents the create command
+	var createCmd = &cobra.Command{
+		Use:   "create",
+		Short: "Create overrides in the given schedule",
+		Long: `This command is used to create overrides in the specified schedule.
+
+Overrides are created using the following rules:
+
+1. Each override is of length "shift-length"
+2. Each override will be created after the "from" parameter
+3. All sets of overrides except the last set will be create before the "to" parameter
+4. Each set of overrides will be "shift-days" number of days long
+5. The gap between sets of overrides will be "gap-days" long
+6. The first shift will start from the time provided at "from"
+`,
+		Run: func(cmd *cobra.Command, args []string) {
+			fromTime, err := time.Parse(time.RFC3339, from)
+			if err != nil {
+				fmt.Printf("could not parse 'from' time: %s; error: %v", from, err)
+				return
+			}
+			toTime, err := time.Parse(time.RFC3339, to)
+			if err != nil {
+				fmt.Printf("could not parse 'to' time: %s; error: %v", to, err)
+				return
+			}
+
+			currentTime := fromTime
+			var overridesToCreate []pagerduty.ShiftTimings
+			for currentTime.Before(toTime) {
+				for i := 0; i < shiftDays; i++ {
+					shiftTimeFrom := currentTime
+					shiftTimeTo := currentTime.Add(shiftLength)
+					overridesToCreate = append(overridesToCreate, pagerduty.ShiftTimings{
+						From: shiftTimeFrom,
+						To:   shiftTimeTo,
+					})
+
+					currentTime = currentTime.Add(24 * time.Hour)
+				}
+
+				currentTime = currentTime.Add(time.Duration(gapDays) * 24 * time.Hour)
+			}
+
+			createdShifts := pagerduty.CreateOverrides(overridesToCreate, userID, scheduleID, dryRun)
+			for i, cs := range createdShifts {
+				output := []string{
+					overridesToCreate[i].From.Format(time.RFC3339),
+					" -> ",
+					overridesToCreate[i].To.Format(time.RFC3339),
+				}
+				if cs.Error != nil {
+					output = append(output, "ERROR", cs.Error.Error())
+				} else {
+					if dryRun {
+						output = append(output, "DRY-RUN")
+					} else {
+						output = append(output, cs.Shift.ID)
+					}
+				}
+				fmt.Println(strings.Join(output, "\t"))
+			}
+		},
+	}
+
+	rootCmd.AddCommand(createCmd)
+
+	createCmd.Flags().StringVarP(&userID, "user-id", "", "", "User who will take these overrides")
+	createCmd.Flags().StringVarP(&scheduleID, "schedule-id", "", "", "Schedule where the overrides should be created")
+	createCmd.Flags().StringVarP(&from, "from", "", "", "From time to start creating overrides")
+	createCmd.Flags().StringVarP(&to, "to", "", "", "Maximum time until which the overrides might be created")
+	createCmd.Flags().DurationVarP(&shiftLength, "shift-length", "", time.Hour, "Length of each shift")
+	createCmd.Flags().IntVarP(&shiftDays, "shift-days", "", 0, "Days of each shift")
+	createCmd.Flags().IntVarP(&gapDays, "gap-days", "", 0, "Gap between the set of days for each shift")
+	createCmd.MarkFlagsRequiredTogether("from", "to", "shift-length", "shift-days", "gap-days", "user-id", "schedule-id")
+	createCmd.Flags().BoolVarP(&dryRun, "dry-run", "n", false, "Run this script without creating anything on PagerDuty")
+}
--- internal/pagerduty/overrides.go
+++ internal/pagerduty/overrides.go
@@ -9,11 +9,20 @@ import (
 	"github.com/PagerDuty/go-pagerduty"
 )
 
+type ShiftTimings struct {
+	From, To time.Time
+}
+
+type CreatedShifts struct {
+	Shift *pagerduty.Override
+	Error error
+}
+
 var authToken string = os.Getenv("AUTHORIZATION_TOKEN")
+var cli *pagerduty.Client = pagerduty.NewClient(authToken)
 
 // ListOverrides ...
 func ListOverrides(from, to time.Time, scheduleID string) []pagerduty.Override {
-	cli := pagerduty.NewClient(authToken)
 	res, err := cli.ListOverridesWithContext(context.Background(), scheduleID, pagerduty.ListOverridesOptions{
 		Since: from.Format(time.RFC3339),
 		Until: to.Format(time.RFC3339),
@@ -24,4 +33,31 @@ func ListOverrides(from, to time.Time, scheduleID string) []pagerduty.Override {
 	}
 
 	return res.Overrides
+}
+
+func CreateOverrides(shifts []ShiftTimings, userID, scheduleID string, dryRun bool) []CreatedShifts {
+	output := make([]CreatedShifts, len(shifts))
+	for i, s := range shifts {
+		overrideToCreate := pagerduty.Override{
+			Start: s.From.Format(time.RFC3339),
+			End:   s.To.Format(time.RFC3339),
+			User: pagerduty.APIObject{
+				ID:   userID,
+				Type: "user_reference",
+			},
+		}
+
+		if dryRun {
+			output[i].Shift = &overrideToCreate
+			continue
+		}
+
+		override, err := cli.CreateOverride(scheduleID, overrideToCreate)
+		if err != nil {
+			output[i].Error = err
+		} else {
+			output[i].Shift = override
+		}
+	}
+	return output
 }
